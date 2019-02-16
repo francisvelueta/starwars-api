826f921cab95506ec782bed89c02a522230d000d
francisvelueta <fvelueta@outlook.com>
2019-02-16 14:30:58 -0600

    add dommy info for layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import AppNavbar from './components/AppNavbar';
+import FilmsList from './components/FilmsList';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavbar/>
+        <FilmsList />
+
       </div>
     );
   }
