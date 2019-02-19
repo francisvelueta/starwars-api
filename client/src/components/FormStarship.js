73f3ab84dd1f44f482aec5298fd0646ea5e679b6
francisvelueta <fvelueta@outlook.com>
2019-02-19 00:17:31 -0600

    add more validations

--- client/src/components/FormStarship.js
+++ client/src/components/FormStarship.js
@@ -14,13 +14,33 @@ class FormStarship extends Component {
     this.state = {
       name:this.props.name,
       model: this.props.model,
-      speed: this.props.speed
+      speed: this.props.speed,
+      inputvalidatename : '',
+      inputvalidatespeed : '',
+      inputvalidatemodel : '',
     }
   }
 
-  sendInfo() {
-    console.log(this.state);
-  }
+sendInfo() {
+if(this.state.name === "" || this.state.name === " ") {
+  this.setState({inputvalidatename: 'is-invalid'})
+} else if (this.state.model === "" || this.state.model === " ") {
+
+  this.setState({inputvalidatemodel: 'is-invalid'})
+
+} else if (this.state.speed === "" || this.state.speed === " ") {
+
+    this.setState({inputvalidatespeed: 'is-invalid'})
+} else {
+  this.setState({inputvalidatename: ''});
+  this.setState({inputvalidatemodel: ''});
+  this.setState({inputvalidatespeed: ''});
+
+  
+}
+
+
+}
 
 
   render() {
@@ -30,20 +50,29 @@ class FormStarship extends Component {
       <Col lg ={6}>
       <FormGroup>
         <Label >Name</Label>
-        <Input type="text" id ="name" name="name" defaultValue = {this.props.name}  onChange = { event => this.setState({ name: event.target.value })} required/>
+        <Input type="text" id ="name" name="name" className={this.state.inputvalidatename} defaultValue = {this.props.name}  onChange = { event => this.setState({ name: event.target.value })} required/>
+        <div className="invalid-feedback">
+          Please write a name.
+        </div>
         </FormGroup>
         </Col>
       </Row>
       <Row><Col lg ={6}>
       <FormGroup>
         <Label >Model</Label>
-        <Input type="text" id="model" name="model" defaultValue ={this.props.model}  onChange = { event => this.setState({ model: event.target.value })} required/>
+        <Input type="text" id="model" name="model" className={this.state.inputvalidatemodel} defaultValue ={this.props.model}  onChange = { event => this.setState({ model: event.target.value })} required/>
+        <div className="invalid-feedback">
+          Please write a model.
+        </div>
       </FormGroup>
       </Col></Row>
       <Row><Col lg ={6}>
       <FormGroup>
         <Label >Speed</Label>
-        <Input type="text" name="speed" defaultValue={this.props.speed} onChange = { event => this.setState({ speed: event.target.value })}  required/>
+        <Input type="text" name="speed" className={this.state.inputvalidatespeed} defaultValue={this.props.speed} onChange = { event => this.setState({ speed: event.target.value })}  required/>
+        <div className="invalid-feedback">
+          Please write a speed.
+        </div>
       </FormGroup>
       </Col></Row>
       <Row><Col lg ={6}>
